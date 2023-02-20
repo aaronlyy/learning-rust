30a66e1d94feab95fced282b2e8d14f9057974ba
aaronlyy <aaronlevican@gmail.com>
2023-02-20 14:06:04 +0100

    added datatypes

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,11 @@ use rand::Rng; // search external packages with 'cargo search <name>' and put pa
 // functions are created with 'fn <name>()'
 // common intendation level is 4 spaces
 fn main() {
+
+}
+
+// basic hello world stuff, user input, format strings
+fn hello_world() {
     println!("What is your name?");
     let mut name = String::new(); // create a mutable variable with 'let mut' & assign an empty string
     let greeting: &str = "Nice to meet you!"; // create immutable variable with 'let' & assign string with &str
@@ -21,5 +26,39 @@ fn main() {
         .expect("No input"); // catch error
 
     // println! is a macro, use '{}' to format, .trim() gets rid of the newline character
-    println!("Hello, {}! {}", name.trim(), greeting); 
+    println!("Hello, {}! {}", name.trim(), greeting);
+}
+
+// shadowing, parsing and constants
+fn shadowing() {
+    const ONE_MIL: u32 = 1_000_000; // create a constant, pass a type with ':'
+    const PI: f32 = 3.141592; // consts with type f32
+    let age: &str = "47"; // immutable string
+
+    // mutable u32 with the same name, called shadowing
+    let mut age: u32 = age.trim().parse()
+        .expect("Age could not be parsed");
+    age = age + 1;
+    println!("I am {} and i got {}$", age, ONE_MIL);
+}
+
+fn datatypes() {
+    // rust is statically typed, can be auto generated
+    // unsigned integers: u8, u16, u32, u64, u128, usize (64bit on 64bit cpu)
+    // signed integer: i8, i16, i32, i64, i128, isize
+    // floats: f32, f64
+    println!("Max u32: {}", u32::MAX); // get max value with ::MAX
+    println!("Max usize: {}", usize::MAX); 
+    println!("Max f32: {}", f32::MAX);
+
+    // booleans
+    let is_true = false;
+
+    // characters
+    let grade = 'a';
+
+}
+
+fn basic_math() {
+
 }
