96aa8fd16c01ee5c9c2f65442277945d1ac6d1bd
aaronlyy <aaronlevican@gmail.com>
2023-02-20 10:46:43 +0100

    added user input and formatted strings

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,17 @@ use std::io::{Write, BufRead, BufReader, ErrorKind}; // import specific items fr
 use rand::Rng; // search external packages with 'cargo search <name>' and put packages in cargo.toml
 
 
-
+// functions are created with 'fn <name>()'
+// common intendation level is 4 spaces
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut name = String::new(); // create a mutable variable with 'let mut' & assign an empty string
+    let greeting: &str = "Nice to meet you!"; // create immutable variable with 'let' & assign string with &str
+
+    // read a line from the command line, result is moved in buf, &mut because mutable
+    io::stdin().read_line(&mut name) // returns an enum
+        .expect("No input"); // catch error
+
+    // println! is a macro, use '{}' to format, .trim() gets rid of the newline character
+    println!("Hello, {}! {}", name.trim(), greeting); 
 }
